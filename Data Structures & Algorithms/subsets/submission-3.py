f444059ad741b141b16f7bd736b06deab7d1bd39
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-05-27 12:30:12 +0530

    Add: subsets - submission-3

---
+++ Data Structures & Algorithms/subsets/submission-3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [] #actual result
+        subset = [] #current branch
+
+        def dfs(idx):
+            #base case
+            if idx == len(nums):
+                res.append(subset.copy())#handover to result
+                return 
+            #add to list from array#INCLUDE
+            subset.append(nums[idx])
+            dfs(idx+1)#move to next branch
+            subset.pop()#backtrack last value
+            
+            #EXCLUDE
+            dfs(idx+1)
+
+
+
+        #init tree
+        dfs(0)
+        return res
+
