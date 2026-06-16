b992ced3f89b3d5938961fee82887d98dbb5426d
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-06-16 16:45:59 +0530

    Add: merge-intervals - submission-2

---
+++ Data Structures & Algorithms/merge-intervals/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        mp = defaultdict(int)
+        for start,end in intervals:
+            mp[start]+=1
+            mp[end]-=1
+        
+        have  = 0
+        interval = []
+        res = []
+
+        for i in sorted(mp):
+            if not interval:
+                interval.append(i)
+
+            have+=mp[i]
+            if have == 0:
+                interval.append(i)
+                res.append(interval)
+                interval = []
+        return res
