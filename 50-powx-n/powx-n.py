c0544b32a1161fd9f63a4ae1c252102923786d58
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-06-18 16:24:04 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (54.17%) - LeetSync

--- 50-powx-n/powx-n.py
+++ 50-powx-n/powx-n.py
@@ -3,7 +3,10 @@ class Solution:
         def power(x, n):
             if n == 0:
                 return 1
-
+            if n < 0:
+                x = 1 / x
+                n = -n
+                
             half = power(x, n // 2)
 
             if n % 2 == 0:
@@ -11,7 +14,6 @@ class Solution:
             else:
                 return half * half * x
 
-        if n < 0:
-            return 1 / power(x, -n)
+        
 
         return power(x, n)
