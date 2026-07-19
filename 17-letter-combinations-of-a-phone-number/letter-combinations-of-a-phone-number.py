e9d4e7485f408de32762a3dc7344dcc784d0a7ed
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-07-19 19:25:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (9.06%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        paths = []
+        path = [None]*len(digits)
+        mp = {'2':"abc" , '3' : "def" , '4' : "ghi" , '5': "jkl" , '6': "mno" , '7': "pqrs" , '8':"tuv" , '9':"wxyz"}
+
+        def dfs(i,path):
+            if i >= len(digits):
+                paths.append(path)
+                return 
+            
+            for letter in mp[digits[i]]:
+                dfs(i+1,path+letter)
+            
+            
+        dfs(0,"")
+        return paths
