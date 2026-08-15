b1ae27ab78d83aacd3311f264f890fb2f7be4f48
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-08-15 20:59:53 +0530

    Time: 0 ms (100.00%) | Memory: 27.9 MB (21.43%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,22 +8,46 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
+        if not head or not head.next:
             return
-        nodes = [] 
-        curr=head
-        while curr!=None:
-            nodes.append(curr)
-            curr = curr.next
+
+        # Step 1: Find Middle
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Split List
+        second = slow.next
+        slow.next = None
+
+        # Step 3: Reverse Second Half
+        prev = None
+
+        while second:
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+
+        second = prev
+
+        # Step 4: Merge
+        first = head
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+            second = temp2
+
         
-        p = 0
-        q = len(nodes)-1
-        while p<q:
-            nodes[p].next = nodes[q]
-            p+=1
-            if p>=q:
-                break
-            nodes[q].next = nodes[p]
-            q-=1
-        nodes[p].next = None
+
+
             
