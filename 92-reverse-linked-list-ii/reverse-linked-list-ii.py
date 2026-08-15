09ba430e603520a80b3a5cb8983641602e4937ee
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-08-15 13:40:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (25.32%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.py
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.py
@@ -5,29 +5,19 @@
 #         self.next = next
 class Solution:
     def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
-
-        # Step 1: Create a dummy node to handle edge cases (like left = 1)
-        dummy = ListNode(0)
-        dummy.next = head
         
-        # Step 2: Reach the node just before the 'left' position
-        prev = dummy
-        for _ in range(left - 1):
+        
+        dummy = ListNode(0,head)
+        prev=dummy
+        for _ in range(0,left-1):
             prev = prev.next
-            
-        # Step 3: Start reversing the sublist
-        # 'curr' is the first node to be reversed
-        curr = prev.next 
+
+        curr = prev.next
+        for _ in range(left,right):
+            tmp=curr.next
+            curr.next = tmp.next
+            tmp.next=prev.next
+            prev.next = tmp
+        return dummy.next  
+
         
-        # We need to do (right - left) swaps
-        for _ in range(right - left):
-            # 'temp' is the node we want to pull to the front of the sublist
-            temp = curr.next
-            
-            # Rearrange the pointers to move 'temp' behind 'prev'
-            curr.next = temp.next
-            temp.next = prev.next
-            prev.next = temp
-            
-        # Step 4: Return the new head
-        return dummy.next
