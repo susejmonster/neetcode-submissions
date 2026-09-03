36e7cb363a3a16228cee133207420227f0f1f023
KRBB_PTTY <84973369+susejmonster@users.noreply.github.com>
2026-09-03 11:45:14 +0530

    Time: 39 ms (21.84%) | Memory: 36.9 MB (12.22%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.py
+++ 287-find-the-duplicate-number/find-the-duplicate-number.py
@@ -1,9 +1,10 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        stack = {}
+        
+        freq = {}
+        for i in range(0,len(nums)):
+            freq[nums[i]] = freq.get(nums[i], 0)+1
 
-        for i in nums:
-            stack[i] = stack.get(i,0)+1
-            if stack[i]>1:
-                return i 
-        return None
+            if freq[nums[i]]>1:
+                return nums[i]
+        return -1
